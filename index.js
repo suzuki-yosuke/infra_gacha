c8c7407a831ac51e7039fbb2c661fc3374fa9487
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-06-29 14:02:48 +0900

    after test for REST API

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-exports.gacha = function gacha (event, callback) {
-  console.log(`gacha: ${event.data.message}`);
-  callback();
+/**
+ * Responds to an HTTP request using data from the request body parsed according
+ * to the "content-type" header.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.gacha = function gacha (req, res) {
+  res.status(200).send(`Hello World!`);
 };
